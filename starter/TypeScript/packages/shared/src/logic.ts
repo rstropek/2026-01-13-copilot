950bce94954da10514a3931e6704b4570f70c16f
Rainer Stropek <rainer@software-architects.at>
2026-01-13 10:26:18 +0100

    Add readme

--- starter/TypeScript/packages/shared/src/logic.ts
+++ starter/TypeScript/packages/shared/src/logic.ts
@@ -1,3 +1,8 @@
 export function add(a: number, b: number) {
     return a + b;
 }
+
+// Function that returns x to the power of z
+export function power(x: number, z: number) {
+    return Math.pow(x, z);
+}
---
+++ starter/TypeScript/readme.md
@@ -0,0 +1,13 @@
+Prerequisites:
+
+* Node (LTS)
+
+```sh
+npm install
+
+# Run server
+npm start
+
+# Run client
+npm run start:client
+```
