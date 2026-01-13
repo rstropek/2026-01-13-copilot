7821be6416452cb44abb84b7187368abbeb74135
Rainer Stropek <rainer@software-architects.at>
2026-01-13 14:23:45 +0100

    Some changes (not relevant)

---
+++ starter/TypeScript/AGENTS.md
@@ -0,0 +1,4 @@
+* General intro to your project
+* Structure of your repository
+* General overview about the used tech
+* Important commands to deal with code
--- starter/TypeScript/packages/shared/src/logic.ts
+++ starter/TypeScript/packages/shared/src/logic.ts
@@ -1,8 +1,3 @@
 export function add(a: number, b: number) {
     return a + b;
 }
-
-// Function that returns x to the power of z
-export function power(x: number, z: number) {
-    return Math.pow(x, z);
-}
