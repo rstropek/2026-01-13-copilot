3e29f4ccabf41dbbc8099dfec8491818c4fe40fe
Rainer Stropek <rainer@software-architects.at>
2026-01-13 11:45:35 +0100

    Add data model for configurable parameters in machines

---
+++ starter/TypeScript/packages/shared/src/providers/configurable.ts
@@ -0,0 +1,18 @@
+// Setup a data model for configurable parameters for machines
+
+export enum SettingType {
+  STRING = 'string',
+  NUMBER = 'number',
+  BOOLEAN = 'boolean',
+}
+
+export enum UnitOfMeasure {
+    DEGREE_CELSIUS = '°C',
+    DEGREE_FAHRENHEIT = '°F',
+    BAR = 'bar',
+    PSI = 'psi',
+    RPM = 'rpm',
+    RPS = 'rps',
+    SECONDS = 'seconds',
+    MINUTES = 'minutes',
+}
